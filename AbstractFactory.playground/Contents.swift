7660b5f97c05d821d0d996e36235e6ec3a4122d4
Konstantin Shendenkov <shendenkov23@gmail.com>
2019-08-18 00:27:01 +0300

    Abstract factory

---
+++ AbstractFactory.playground/Contents.swift
@@ -0,0 +1,86 @@
+protocol AbstractFactory {
+  func createFootman() -> AbstractFootman
+  func createArcher() -> AbstractArcher
+}
+
+protocol AbstractFootman {
+  func fight()
+}
+
+extension AbstractFootman {
+  func fight() {
+    print("\(type(of: self)) fight!")
+  }
+}
+
+protocol AbstractArcher {
+  func fire()
+}
+
+extension AbstractArcher {
+  func fire() {
+    print("\(type(of: self)) fire!")
+  }
+}
+
+// MARK: - Factories
+
+class RedFactory: AbstractFactory {
+  public func createFootman() -> AbstractFootman {
+    return RedFootman()
+  }
+
+  public func createArcher() -> AbstractArcher {
+    return RedArcher()
+  }
+}
+
+class BlueFactory: AbstractFactory {
+  public func createFootman() -> AbstractFootman {
+    return BlueFootman()
+  }
+
+  public func createArcher() -> AbstractArcher {
+    return BlueArcher()
+  }
+}
+
+// MARK: - RED TEAM
+
+class RedFootman: AbstractFootman {}
+
+class RedArcher: AbstractArcher {}
+
+// MARK: - BLUE TEAM
+
+class BlueFootman: AbstractFootman {}
+
+class BlueArcher: AbstractArcher {}
+
+// MARK: -
+
+class Player {
+  private let footman: AbstractFootman
+  private let archer: AbstractArcher
+
+  public init(factory: AbstractFactory) {
+    footman = factory.createFootman()
+    archer = factory.createArcher()
+  }
+
+  public func run() {
+    footman.fight()
+    archer.fire()
+  }
+}
+
+
+// MARK: - Start
+
+let redFactory = RedFactory()
+let redPlayer = Player(factory: redFactory)
+redPlayer.run()
+
+let blueFactory = BlueFactory()
+let bluePlayer = Player(factory: blueFactory)
+bluePlayer.run()
